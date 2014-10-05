40fc87af51e3113cf617c0f6dd6ef244dea39a35
Benny Neugebauer <bn@bennyn.de>
2014-10-05 14:50:29 +0200

    "end_of_line" & "insert_final_newline" can be read.

--- src/main/java/org/editorconfig/netbeans/test/MainClass.java
+++ src/main/java/org/editorconfig/netbeans/test/MainClass.java
@@ -2,11 +2,16 @@ package org.editorconfig.netbeans.test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 import org.editorconfig.core.EditorConfig;
+import org.editorconfig.core.EditorConfigException;
+import org.openide.util.Exceptions;
 
 public class MainClass {
 
-  public static void main(String[] args) throws Exception {
+  private static final Logger LOG = Logger.getLogger(MainClass.class.getName());
+
+  public static void main(String[] args) {
     /*
      EditorConfigParser parser = new EditorConfigParser();
      String result = parser.parseResource("editorconfig-test.ini");
@@ -21,10 +26,19 @@ public class MainClass {
     filePaths.add(path);
 
     for (String filePath : filePaths) {
-      EditorConfig editorConfig = new EditorConfig();
-      List<EditorConfig.OutPair> properties = editorConfig.getProperties(filePath);
-      System.out.println("Properties found: " + properties.size());
-    }
-  }
+      EditorConfig editorConfig = new EditorConfig("editorconfig-test.ini", EditorConfig.VERSION);
+      try {
+        List<EditorConfig.OutPair> properties = editorConfig.getProperties(filePath);
 
+        for (int i = 0; i < properties.size(); ++i) {
+          EditorConfig.OutPair property = properties.get(i);
+          System.out.println("Key: " + property.getKey());
+          System.out.println("Value: " + property.getVal());
+        }
+      } catch (EditorConfigException ex) {
+        Exceptions.printStackTrace(ex);
+      }
+    }
+
+  }
 }
