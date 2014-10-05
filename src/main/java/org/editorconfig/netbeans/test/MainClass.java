9621118a8f0a6239cc77b7f194b93a13ab586b33
Benny Neugebauer <bn@bennyn.de>
2014-10-05 18:04:45 +0200

    Implemented a RegEx test to match certain rules.

--- src/main/java/org/editorconfig/netbeans/test/MainClass.java
+++ src/main/java/org/editorconfig/netbeans/test/MainClass.java
@@ -6,6 +6,8 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.editorconfig.core.EditorConfig;
 import org.editorconfig.core.EditorConfigException;
 import org.editorconfig.core.OutPair;
@@ -16,16 +18,20 @@ public class MainClass {
   private static final Logger LOG = Logger.getLogger(MainClass.class.getName());
 
   public static void main(String[] args) throws URISyntaxException {
+    Pattern pattern = Pattern.compile(".*py$");
+    Matcher matcher = pattern.matcher("myfile.py");
+    boolean isMatched = matcher.matches();
+    System.out.println("Is matched: " + isMatched);
+
     /*
      EditorConfigParser parser = new EditorConfigParser();
      String result = parser.parseResource("editorconfig-test.ini");
      System.out.println(result);
      */
-
     List<String> filePaths = new ArrayList<>();
 
     // Get test file
-    String testFilePath = "org/editorconfig/example/editorconfig-test.ini";
+    String testFilePath = "org/editorconfig/example/.editorconfig";
     ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
     // Absolute Path with forward slashes
     URL resource = classLoader.getResource(testFilePath);
