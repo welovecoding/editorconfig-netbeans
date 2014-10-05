b45a9cdc439784d80db719402851b783b05f8a01
Benny Neugebauer <bn@bennyn.de>
2014-10-05 21:55:04 +0200

    Own EditorConfig parser works great!

--- src/main/java/org/editorconfig/netbeans/parser/EditorConfigParser.java
+++ src/main/java/org/editorconfig/netbeans/parser/EditorConfigParser.java
@@ -6,46 +6,72 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class EditorConfigParser {
-  
+
   private static final Logger LOG = Logger.getLogger(EditorConfigParser.class.getName());
 
-  // https://github.com/editorconfig/editorconfig-core-java/blob/master/src/org/editorconfig/core/EditorConfig.java
   public EditorConfigParser() {
   }
-  
-  public void parseConfig(URL resource) {
+
+  public Map<String, List<EditorConfigProperty>> parseConfig(URL resource) {
+    Map<String, List<EditorConfigProperty>> result = new HashMap<>();
+    String section = null;
+
     File file = new File(resource.getFile());
-    StringBuilder sb = new StringBuilder();
     String line;
-    
+
     try (
             FileInputStream fis = new FileInputStream(file);
             InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
             BufferedReader br = new BufferedReader(isr)) {
       while ((line = br.readLine()) != null) {
         boolean isInteresting = !(line.startsWith("#") || line.isEmpty());
-        
+
         if (isInteresting) {
           if (line.startsWith("[")) {
             // RegEx
             String regex = line.substring(1, line.lastIndexOf("]"));
-            System.out.println("REGEX: " + regex);
             // TODO: Convert RegEx to Java compliant RegEx
-          } else {
+            section = regex;
+          } else if (section != null) {
             // Key / Value
             String[] splitted = line.split("=");
-            System.out.println("Key: " + splitted[0].trim());
-            System.out.println("Value: " + splitted[1].trim());
+            String key = splitted[0].trim();
+            String value = splitted[1].trim();
             // TODO: Save Key / Value pairs together with RegEx in result set
+            List<EditorConfigProperty> properties = result.get(section);
+
+            if (properties == null) {
+              properties = new ArrayList<>();
+              result.put(section, properties);
+            }
+
+            properties.add(new EditorConfigProperty(key, value));
           }
         }
       }
     } catch (IOException ex) {
       LOG.log(Level.SEVERE, "Error reading file: {0}", ex.getMessage());
+    }
+
+    return result;
+  }
+
+  public void printConfig(Map<String, List<EditorConfigProperty>> config) {
+    for (String section : config.keySet()) {
+      System.out.println("Section: " + section);
+      List<EditorConfigProperty> properties = config.get(section);
+      for (EditorConfigProperty property : properties) {
+        String output = String.format("\t%s: %s", property.getKey(), property.getValue());
+        System.out.println(output);
+      }
     }
   }
 }
--- src/main/java/org/editorconfig/netbeans/test/MainClass.java
+++ src/main/java/org/editorconfig/netbeans/test/MainClass.java
@@ -1,18 +1,14 @@
 package org.editorconfig.netbeans.test;
 
-import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import org.editorconfig.core.EditorConfig;
-import org.editorconfig.core.EditorConfigException;
-import org.editorconfig.core.OutPair;
 import org.editorconfig.netbeans.parser.EditorConfigParser;
-import org.openide.util.Exceptions;
+import org.editorconfig.netbeans.parser.EditorConfigProperty;
 
 public class MainClass {
 
@@ -30,6 +26,7 @@ public class MainClass {
     URL resource = classLoader.getResource(testFilePath);
 
     EditorConfigParser parser = new EditorConfigParser();
-    parser.parseConfig(resource);
+    Map<String, List<EditorConfigProperty>> config = parser.parseConfig(resource);
+    parser.printConfig(config);
   }
 }
