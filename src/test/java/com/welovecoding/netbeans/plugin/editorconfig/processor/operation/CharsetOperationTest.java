6e13427eb0ffded566262b31be1b80df9723cb96
Benny Neugebauer <bn@bennyn.de>
2014-11-13 20:28:41 +0100

    Setup Master Filesystem.

--- src/test/java/com/welovecoding/netbeans/plugin/editorconfig/processor/operation/CharsetOperationTest.java
+++ src/test/java/com/welovecoding/netbeans/plugin/editorconfig/processor/operation/CharsetOperationTest.java
@@ -3,6 +3,11 @@ package com.welovecoding.netbeans.plugin.editorconfig.processor.operation;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.netbeans.junit.NbTestCase;
@@ -17,10 +22,12 @@ public class CharsetOperationTest extends NbTestCase {
 
   @Test
   @Ignore
-  public void testSetup() throws FileNotFoundException, IOException {
-    File file = new File("files/utf-8-bom.txt");
-    FileObject fo = FileUtil.toFileObject(file.getAbsoluteFile());
-    assertEquals("", "");
+  public void testSetup() throws FileNotFoundException, IOException, URISyntaxException {
+    String path = "files/charsets/utf-8-bom.txt";
+    URL url = Thread.currentThread().getContextClassLoader().getResource(path);
+    Path testFilePath = Paths.get(url.toURI());
+    FileObject fo = FileUtil.toFileObject(testFilePath.toFile());
+    assertEquals(fo.getName(), "utf-8-bom");
   }
 
 }
