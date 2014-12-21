6ddb6ddbdb19a641fc55cf765d368713dc80e330
Benny Neugebauer <bn@bennyn.de>
2014-12-22 00:02:40 +0100

    Added FileObject reader.

---
+++ src/main/java/com/welovecoding/netbeans/plugin/editorconfig/io/reader/FileObjectReader.java
@@ -0,0 +1,33 @@
+package com.welovecoding.netbeans.plugin.editorconfig.io.reader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import org.netbeans.api.queries.FileEncodingQuery;
+import org.openide.filesystems.FileObject;
+import org.openide.util.Exceptions;
+
+public class FileObjectReader {
+
+  public static synchronized String read(FileObject fo) {
+    return FileObjectReader.read(fo, FileEncodingQuery.getEncoding(fo));
+  }
+
+  public static synchronized String read(FileObject fo, Charset cs) {
+    final StringBuilder sb = new StringBuilder();
+    final char[] buffer = new char[512];
+
+    try (Reader in = new InputStreamReader(fo.getInputStream(), cs)) {
+      int len;
+      while ((len = in.read(buffer)) > 0) {
+        sb.append(buffer, 0, len);
+      }
+    } catch (IOException ex) {
+      Exceptions.printStackTrace(ex);
+    }
+
+    return sb.toString();
+  }
+
+}
